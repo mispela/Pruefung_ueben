7806d3f6f7099e33b8d395841e010d6c885e7975
Alexander Mispelhorn <Alexandermis@web.de>
2021-01-07 19:14:50 +0100

    Teilen durch 3 mit rand

---
+++ Pruefung.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <time.h>
+int durch_3_teilbar(int *b, int i);
+int durch_3_teilbar(int *b, int i)
+{
+
+	static int k=-1;
+	k++;
+	if(k==i)
+	{
+		return -998;
+	}
+	if((*(b+k)%3)==0)
+	{
+		return*(b+k);
+	}
+	
+	return -1;
+}
+
+
+int main () 
+{
+	int m;
+	srand(time(NULL));
+	int array_size =rand()%10;
+	printf("array_size=%d\n",array_size);
+	int *b = malloc(array_size * sizeof(int));
+	if(b==NULL)
+	{
+		printf("No ram availalbe");
+	}
+	int r;
+	for(int i=0;i<array_size;i++)
+	{
+		r=rand()%2?rand()%100:rand()%100*-1;
+		*(b+i)=r;
+	}
+	int d=-1;
+	while((m=durch_3_teilbar(b, array_size))!=-998)
+	{
+		d++;
+		if(m!=-1)
+		{
+			printf("%d ist durch 3 teilbar\n",m);
+		}
+		else
+		{
+			printf("%d ist NICHT durch 3 Teilbar\n",*(b+d));
+		}
+	}
+	b=NULL;
+	free(b);
+return 0;
+}
